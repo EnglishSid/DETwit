fbba9c1ce8057b146a416808ca18a4d52099259e
David Stevens <david.stevens@hotmail.com>
2018-01-17 19:40:46 +0000

    Delete twitconfig.js

--- twitconfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-   
-    consumer_key:         'X9JSxj8BxIA074Y5QtZh98gxN',
-    consumer_secret:      'XzsWp9oOSW0yH8lspk4rr0LvGDnuO7iEZt42LZ2yt0ZP5E68uG',
-    access_token:         '15426187-XwRkXgcZeZM6LS7TyboRirRs0cPAVzYz7OqCakfrr',
-    access_token_secret:  'yvr6oz2X409DissltEKrkzYuBi9cmCStJV75VykXvfVag',
-}
